4bbc11fa528474f1988b9de58739626a021c78b7
Ubuntu <ubuntu@ip-172-31-36-152.ap-southeast-1.compute.internal>
2026-07-31 12:50:12 +0000

    update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("CI/CD deployment successful!");
-});
+  res.send("CI/CD deployment success full lab CICD!");
+};
 
 app.listen(port, () => {
   console.log(`Application running at port ${port}`);
