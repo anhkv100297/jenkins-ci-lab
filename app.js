eca7c831da739a09e9ae1d2fca40be6270630ddb
anhkv-ci <anhkv100297@gmail.com>
2026-08-04 04:55:46 +0000

    upgrade app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
 
         <!-- Headline -->
         <h1 class="text-3xl font-semibold tracking-tight text-[#1d1d1f] mb-3">
-         Owner Vanh - CI/CD Deployment
+         CI/CD Deployment
         </h1>
         
         <!-- Subtitle Gradient -->
